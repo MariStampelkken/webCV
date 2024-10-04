1dfe52769f35d7db9ae7fcede579f5c185670976
Mari Stampeløkken <marstamp@stud.ntnu.no>
2024-10-04 09:16:17 +0200

    feat: set up routes
    routes for home page and about page set up
    
    issue #2

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './home/Home.tsx';
+import About from './about/About.tsx';
+
+const RouterConfig = () => (
+  <div data-testid="auth-wrapper">
+    <div style={{ minHeight: '100vh' }}>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="*"
+          element={<h1 style={{ textAlign: 'center' }}>404 Page Not Found</h1>}
+        />
+      </Routes>
+    </div>
+  </div>
+);
+
+export default RouterConfig;
